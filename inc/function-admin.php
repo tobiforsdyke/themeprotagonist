f00a4a05b9b382042b2ed15d2c1910b9364f4d3b
tobiforsdyke <tobiforsdyke@gmail.com>
2019-02-13 13:22:03 +0000

    Added the function to register custom settings in the database. Started adding fields. Added a field for first name and the function to store the setting.

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -13,8 +13,26 @@ function protagonist_add_admin_page() {
   add_submenu_page( 'protagonist_adminpage', 'Protagonist Options', 'General', 'manage_options', 'protagonist_adminpage', 'protagonist_theme_create_page' );
   add_submenu_page( 'protagonist_adminpage', 'Protagonist Colour Themes', 'Colour Themes', 'manage_options', 'protagonist_adminpage_colourthemes', 'protagonist_theme_colour_settings_page' );
   add_submenu_page( 'protagonist_adminpage', 'Protagonist CSS Options', 'Custom CSS', 'manage_options', 'protagonist_adminpage_css', 'protagonist_theme_css_settings_page' );
+  // Activate custom settings
+  add_action( 'admin_init', 'protagonist_custom_settings' );
 }
 add_action( 'admin_menu', 'protagonist_add_admin_page' );
+
+function protagonist_custom_settings() {
+  // Registers settings in the database
+  register_setting( 'protagonist-settings-group', 'first_name' );
+  add_settings_section( 'protagonist-sidebar-options', 'Sidebar Options', 'protagonist_sidebar_options', 'protagonist_adminpage' );
+  add_settings_field( 'sidebar-name', 'First Name', 'protagonist_sidebar_name', 'protagonist_adminpage', 'protagonist-sidebar-options' );
+}
+
+function protagonist_sidebar_options() {
+  echo 'Customise your sidebar information.';
+}
+
+function protagonist_sidebar_name() {
+  $firstName = esc_attr( get_option( 'first_name' ) );
+  echo '<input type="text" name="first_name" value="'.$firstName.'" placeholder="First Name" />';
+}
 
 function protagonist_theme_create_page() {
   // Generation of the admin oage
--- inc/templates/protagonist-admin.php
+++ inc/templates/protagonist-admin.php
@@ -1,3 +1,7 @@
 <h1>Protagonist Theme Options</h1>
-<h3>Manage Options</h3>
-<p>Customise the default Wordpress appearance.</p>
+<?php settings_errors(); ?>
+<form action="options.php" method="post">
+  <?php settings_fields( 'protagonist-settings-group' ); ?>
+  <?php do_settings_sections( 'protagonist_adminpage' ); ?>
+  <?php submit_button(); ?>
+</form>
