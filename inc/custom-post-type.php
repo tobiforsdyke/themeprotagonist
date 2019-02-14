2788b2f2c0f9206611150b3c1c80ebcdee01134a
tobiforsdyke <tobiforsdyke@gmail.com>
2019-02-14 16:51:40 +0000

    Additional contact form edits

--- inc/custom-post-type.php
+++ inc/custom-post-type.php
@@ -8,7 +8,11 @@
 
 $contact = get_option( 'activate_contact' );
 if( @$contact == 1 ) {
+
   add_action( 'init', 'protagonist_contact_custom_post_type' );
+  add_filter( 'manage_protagonist-contact_posts_columns', 'protagonist_set_contact_columns' );
+  add_action( 'manage_protagonist-contact_posts_custom_column', 'protagonist_contact_custom_column', 10, 2 );
+
 }
 
 // CONTACT CUSTOM POST TYPE
@@ -32,4 +36,24 @@ function protagonist_contact_custom_post_type() {
   );
 
   register_post_type( 'protagonist-contact', $args );
+}
+
+function protagonist_set_contact_columns( $columns ) {
+  $newColumns = array();
+  $newColumns['title'] = 'Full Name';
+  $newColumns['message'] = 'Message';
+  $newColumns['email'] = 'Email';
+  $newColumns['date'] = 'Date';
+  return $newColumns;
+}
+
+function protagonist_contact_custom_column( $column, $post_id ) {
+  switch( $column ){
+    case 'message' :
+      echo get_the_excerpt();
+      break;
+    case 'email' :
+      // echo 'email address';
+      break;
+  }
 }
