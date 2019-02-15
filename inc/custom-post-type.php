8572af7bd182a6b7ed2b55dd2c2f006d24ffd372
tobiforsdyke <tobiforsdyke@gmail.com>
2019-02-15 12:24:21 +0000

    Added a custom meta box for the contact forms email address field

--- inc/custom-post-type.php
+++ inc/custom-post-type.php
@@ -12,6 +12,8 @@ if( @$contact == 1 ) {
   add_action( 'init', 'protagonist_contact_custom_post_type' );
   add_filter( 'manage_protagonist-contact_posts_columns', 'protagonist_set_contact_columns' );
   add_action( 'manage_protagonist-contact_posts_custom_column', 'protagonist_contact_custom_column', 10, 2 );
+  add_action( 'add_meta_boxes', 'protagonist_add_contact_meta_box' );
+  add_action( 'save_post', 'protagonist_save_contact_email_data' );
 
 }
 
@@ -53,7 +55,40 @@ function protagonist_contact_custom_column( $column, $post_id ) {
       echo get_the_excerpt();
       break;
     case 'email' :
-      // echo 'email address';
+      $email = get_post_meta( $post_id, '_contact_email_value_key', true );
+      echo '<a href="mailto:'.$email.'">'.$email.'</a>';
       break;
   }
+}
+
+// CONTACT META BOXES
+function protagonist_add_contact_meta_box() {
+  add_meta_box( 'contact_email', 'Email Address', 'protagonist_contact_email_callback', 'protagonist-contact', 'side', 'high' );
+}
+
+function protagonist_contact_email_callback( $post ) {
+  wp_nonce_field( 'protagonist_save_contact_email_data', 'protagonist_contact_email_meta_box_nonce' );
+  $value = get_post_meta( $post->ID, '_contact_email_value_key', true );
+  echo '<label for="protagonist_contact_email_field">Uer Email Address: </label>';
+  echo '<input type="email" id="protagonist_contact_email_field" name="protagonist_contact_email_field" value="' . esc_attr( $value ) . '" size="25" />';
+}
+
+function protagonist_save_contact_email_data( $post_id ) {
+  if( ! isset( $_POST['protagonist_contact_email_meta_box_nonce'] ) ){
+    return;
+  }
+  if( ! wp_verify_nonce( $_POST['protagonist_contact_email_meta_box_nonce'], 'protagonist_save_contact_email_data' ) ){
+    return;
+  }
+  if( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ){
+    return;
+  }
+  if( ! current_user_can( 'edit_post', $post_id ) ){
+    return;
+  }
+  if( ! isset( $_POST['protagonist_contact_email_field'] ) ){
+    return;
+  }
+  $my_data = sanitize_text_field( $_POST['protagonist_contact_email_field'] );
+  update_post_meta( $post_id, '_contact_email_value_key', $my_data );
 }
